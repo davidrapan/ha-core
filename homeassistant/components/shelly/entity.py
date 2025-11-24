fc735ed47d919771660061cb524ce64685471477
David Rapan <david@rapan.cz>
2025-11-24 23:03:56 +0100

    hm
    Signed-off-by: David Rapan <david@rapan.cz>

--- homeassistant/components/shelly/entity.py
+++ homeassistant/components/shelly/entity.py
@@ -25,7 +25,6 @@ from .coordinator import ShellyBlockCoordinator, ShellyConfigEntry, ShellyRpcCoo
 from .utils import (
     async_remove_shelly_entity,
     get_block_device_info,
-    get_entity_translation_attributes,
     get_rpc_channel_name,
     get_rpc_device_info,
     get_rpc_key,
@@ -596,34 +595,16 @@ class ShellyRpcAttributeEntity(ShellyRpcEntity, Entity):
 
         return self.entity_description.available(self.sub_status)
 
-
-class ShellyRpcAttributeChannelEntity(ShellyRpcAttributeEntity):
-    """Helper class to represent a rpc attribute."""
-
-    def __init__(
-        self,
-        coordinator: ShellyRpcCoordinator,
-        key: str,
-        attribute: str,
-        description: RpcEntityDescription,
-    ) -> None:
-        """Initialize sensor."""
-        super().__init__(coordinator, key, attribute, description)
-
-        if not description.role:
-            translation_placeholders, translation_key = (
-                get_entity_translation_attributes(
-                    get_rpc_channel_name(coordinator.device, key),
-                    description.translation_key,
-                    description.device_class,
-                    self._default_to_device_class_name(),
-                )
-            )
-
-            if translation_placeholders:
-                self._attr_translation_placeholders = translation_placeholders
-                if translation_key:
-                    self._attr_translation_key = translation_key
+    def configure_translation_attributes(self) -> None:
+        """Configure translation attributes."""
+        if channel_name := get_rpc_channel_name(self.coordinator.device, self.key):
+            if key := self.entity_description.translation_key or (
+                self.entity_description.device_class
+                if self._default_to_device_class_name()
+                else None
+            ):
+                self._attr_translation_placeholders = {"channel_name": channel_name}
+                self._attr_translation_key = f"{key}_with_channel_name"
 
 
 class ShellySleepingBlockAttributeEntity(ShellyBlockAttributeEntity):
@@ -720,36 +701,6 @@ class ShellySleepingRpcAttributeEntity(ShellyRpcAttributeEntity):
             "Entity %s comes from a sleeping device, update is not possible",
             self.entity_id,
         )
-
-
-class ShellySleepingRpcAttributeChannelEntity(ShellySleepingRpcAttributeEntity):
-    """Helper class to represent a sleeping rpc attribute."""
-
-    def __init__(
-        self,
-        coordinator: ShellyRpcCoordinator,
-        key: str,
-        attribute: str,
-        description: RpcEntityDescription,
-        entry: RegistryEntry | None = None,
-    ) -> None:
-        """Initialize the sleeping sensor."""
-        super().__init__(coordinator, key, attribute, description, entry)
-
-        if coordinator.device.initialized:
-            translation_placeholders, translation_key = (
-                get_entity_translation_attributes(
-                    get_rpc_channel_name(coordinator.device, key),
-                    description.translation_key,
-                    description.device_class,
-                    self._default_to_device_class_name(),
-                )
-            )
-
-            if translation_placeholders:
-                self._attr_translation_placeholders = translation_placeholders
-                if translation_key:
-                    self._attr_translation_key = translation_key
 
 
 def get_entity_class(
