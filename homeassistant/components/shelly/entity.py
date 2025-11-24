e986b8d139802a2ad9e3d2cc1a42c050fa603a29
David Rapan <david@rapan.cz>
2025-11-24 23:10:32 +0100

    heh
    Signed-off-by: David Rapan <david@rapan.cz>

--- homeassistant/components/shelly/entity.py
+++ homeassistant/components/shelly/entity.py
@@ -597,14 +597,17 @@ class ShellyRpcAttributeEntity(ShellyRpcEntity, Entity):
 
     def configure_translation_attributes(self) -> None:
         """Configure translation attributes."""
-        if channel_name := get_rpc_channel_name(self.coordinator.device, self.key):
-            if key := self.entity_description.translation_key or (
+        if (
+            channel_name := get_rpc_channel_name(self.coordinator.device, self.key)
+        ) and (
+            key := self.entity_description.translation_key or (
                 self.entity_description.device_class
                 if self._default_to_device_class_name()
                 else None
-            ):
-                self._attr_translation_placeholders = {"channel_name": channel_name}
-                self._attr_translation_key = f"{key}_with_channel_name"
+            )
+        ):
+            self._attr_translation_placeholders = {"channel_name": channel_name}
+            self._attr_translation_key = f"{key}_with_channel_name"
 
 
 class ShellySleepingBlockAttributeEntity(ShellyBlockAttributeEntity):
